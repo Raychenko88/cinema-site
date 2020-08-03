fcf3749cfaeb3057fea1c2c5f4157604139b2d27
Sergey <sergeyraychenko@gmail.com>
2020-08-03 14:19:17 +0300

    add all methods for UserServiceImpl

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -5,11 +5,11 @@ import org.example.model.User;
 import java.util.List;
 
 public interface UserService {
-    User save(User user);
-    User findById(Integer id);
-    User update(User user);
+    User save(User user) throws Exception;
+    User findById(Integer id) throws Exception;
+    User update(User user) throws Exception;
     void delete(User user);
-    List<User> findAll();
-    User findByLogin(String login);
-    User findByLoginAndPassword(String login, String password);
+    List<User> findAll() throws Exception;
+    User findByLogin(String login) throws Exception;
+    User findByLoginAndPassword(String login, String password) throws Exception;
 }
--- src/main/java/org/example/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -1,5 +1,64 @@
 package org.example.service.impl;
 
-public class UserServiceImpl {
-    
+import org.example.dao.UserDAO;
+import org.example.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Override
+    public User save(User user) throws Exception {
+        if (user.getLogin() != null && userDAO.findByLogin(user.getLogin()) != null){
+            throw new Exception("user with this login already exists");
+        }
+        return userDAO.save(user);
+    }
+
+    @Override
+    public User findById(Integer id) throws Exception {
+        return userDAO.findById(id).orElseThrow(() -> new Exception("user not found"));
+    }
+
+    @Override
+    public User update(User user) throws Exception {
+        if (user.getId() == null || userDAO.findById(user.getId()) == null){
+            throw new Exception("user id not found");
+        }
+        return userDAO.save(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        userDAO.delete(user);
+    }
+
+    @Override
+    public List<User> findAll() throws Exception {
+        if (userDAO.findAll().isEmpty()){
+            throw new Exception("no users found");
+        }
+        return userDAO.findAll();
+    }
+
+    @Override
+    public User findByLogin(String login) throws Exception {
+        if (userDAO.findByLogin(login) == null){
+            throw new Exception("user with this login was not found");
+        }
+        return userDAO.findByLogin(login);
+    }
+
+    @Override
+    public User findByLoginAndPassword(String login, String password) throws Exception {
+        if (userDAO.findByLoginAndPassword(login,password) == null){
+            throw new Exception("user with this login and password was not found");
+        }
+        return userDAO.findByLoginAndPassword(login, password);
+    }
 }
