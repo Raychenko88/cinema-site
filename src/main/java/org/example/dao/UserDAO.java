8a8458ba34e644996ecc68b307a16ce4c9e6a1f9
Sergey <sergeyraychenko@gmail.com>
2020-08-03 13:50:09 +0300

    add UserService and UserServiceImpl and add methods to UserDAO

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
 
+    User findByLogin(String login);
+    User findByLoginAndPassword(String login, String password);
 }
